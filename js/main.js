560cdeaf487e85face7d9fbead4b9a1ba29d571e
tornikezhizhi <zhizhiashvilitornike@gmail.com>
2020-04-11 04:37:41 +0400

    add slider effect

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
 
 //Language	
 	$(document).on("click",function(){
-		$(".language_dropdown li").slideUp();
+		$(".language_dropdown li").slideUp(50);
 		$(".active_language").removeClass("active");
 	})
 
@@ -42,14 +42,47 @@ $(document).ready(function() {
 		$(this).addClass("active")
 	})
 
- var swiper = new Swiper('.swiper-container.header_swiper', {
- 	 effect: 'fade',
- 	 loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
+var interleaveOffset = 0.5;
 
+var swiperOptions = {
+  loop: true,
+  speed: 1000,
+  grabCursor: true,
+  watchSlidesProgress: true,
+  mousewheelControl: true,
+  keyboardControl: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  on: {
+    progress: function() {
+      var swiper = this;
+      for (var i = 0; i < swiper.slides.length; i++) {
+        var slideProgress = swiper.slides[i].progress;
+        var innerOffset = swiper.width * interleaveOffset;
+        var innerTranslate = slideProgress * innerOffset;
+        swiper.slides[i].querySelector(".slide-inner").style.transform =
+          "translate3d(" + innerTranslate + "px, 0, 0)";
+      }      
+    },
+    touchStart: function() {
+      var swiper = this;
+      for (var i = 0; i < swiper.slides.length; i++) {
+        swiper.slides[i].style.transition = "";
+      }
+    },
+    setTransition: function(speed) {
+      var swiper = this;
+      for (var i = 0; i < swiper.slides.length; i++) {
+        swiper.slides[i].style.transition = speed + "ms";
+        swiper.slides[i].querySelector(".slide-inner").style.transition =
+          speed + "ms";
+      }
+    }
+  }
+};
+
+var swiper = new Swiper(".swiper-container", swiperOptions);
 	
 })
